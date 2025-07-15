aee4cdd5993d78ce6eb198a716dcae2e321c6e49
virendraPrajapat18 <166205346+virendraPrajapat18@users.noreply.github.com>
2025-07-15 18:49:42 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -37,7 +37,8 @@ function App() {
   const dispatch = useDispatch();
 
   useEffect(()=>{
-    dispatch(checkAuth());
+    const token = JSON.parse(sessionStorage.getItem('token'))
+    dispatch(checkAuth(token));
   },[dispatch]);
 
   if(isLoading) return <Skeleton className="w-[600px] bg-black h-[600px] " />;
@@ -108,3 +109,4 @@ function App() {
 }
 
 export default App;
+
